66cc3dca03368aec3d16b0bd62fa055d84efbf3a
Ercan Ersoy <ercanersoy@ercanersoy.net>
2024-05-26 18:30:47 +0300

    Add initial codes and README.md
    Signed-off-by: Ercan Ersoy <ercanersoy@ercanersoy.net>

---
+++ DOCX Viewer/Window.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Viewer
+{
+    partial class Window_Form
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Open_Button = new System.Windows.Forms.Button();
+            this.Area_RichTextBox = new System.Windows.Forms.RichTextBox();
+            this.Open_File_OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // Open_Button
+            // 
+            this.Open_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Open_Button.Location = new System.Drawing.Point(12, 12);
+            this.Open_Button.Name = "Open_Button";
+            this.Open_Button.Size = new System.Drawing.Size(80, 40);
+            this.Open_Button.TabIndex = 0;
+            this.Open_Button.Text = "Aç";
+            this.Open_Button.UseVisualStyleBackColor = true;
+            this.Open_Button.Click += new System.EventHandler(this.Open_Button_Click);
+            // 
+            // Area_RichTextBox
+            // 
+            this.Area_RichTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Area_RichTextBox.Location = new System.Drawing.Point(0, 58);
+            this.Area_RichTextBox.Name = "Area_RichTextBox";
+            this.Area_RichTextBox.Size = new System.Drawing.Size(800, 392);
+            this.Area_RichTextBox.TabIndex = 1;
+            this.Area_RichTextBox.Text = "";
+            // 
+            // Window_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Area_RichTextBox);
+            this.Controls.Add(this.Open_Button);
+            this.Name = "Window_Form";
+            this.Text = "DOCX Viewer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Open_Button;
+        private System.Windows.Forms.RichTextBox Area_RichTextBox;
+        private System.Windows.Forms.OpenFileDialog Open_File_OpenFileDialog;
+    }
+}
+
---
+++ DOCX Viewer/Window.cs
@@ -0,0 +1,119 @@
+﻿// Copyright (c) 2024 Ercan Ersoy
+//
+// Written by Ercan Ersoy.
+// Also, take advantage of ChatGPT GPT-4o and GitHub Copilot.
+
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Viewer
+{
+    public partial class Window_Form : Form
+    {
+        public Window_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Open_Button_Click(object sender, EventArgs e)
+        {
+            if (Open_File_OpenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string file_path = Open_File_OpenFileDialog.FileName;
+
+                    using (WordprocessingDocument word_document = WordprocessingDocument.Open(file_path, false))
+                    {
+                        Body body = word_document.MainDocumentPart.Document.Body;
+
+                        Area_RichTextBox.Clear();
+
+                        foreach (var paragraph in body.Elements<Paragraph>())
+                        {
+                            if (paragraph.ParagraphProperties != null && paragraph.ParagraphProperties.Justification != null)
+                            {
+                                if(paragraph.ParagraphProperties.Justification.Val == JustificationValues.Center)
+                                {
+                                    Area_RichTextBox.SelectionAlignment = HorizontalAlignment.Center;
+                                }
+                                else if (paragraph.ParagraphProperties.Justification.Val == JustificationValues.Center)
+                                {
+                                    Area_RichTextBox.SelectionAlignment = HorizontalAlignment.Right;
+                                }
+                                else
+                                {
+                                    Area_RichTextBox.SelectionAlignment = HorizontalAlignment.Left;
+                                }
+                            }
+                            else
+                            {
+                                Area_RichTextBox.SelectionAlignment = HorizontalAlignment.Left;
+                            }
+
+                            foreach (var run in paragraph.Elements<DocumentFormat.OpenXml.Wordprocessing.Run>())
+                            {
+                                foreach (var text in run.Elements<DocumentFormat.OpenXml.Wordprocessing.Text>())
+                                {
+                                    int start = Area_RichTextBox.TextLength;
+                                    int end = Area_RichTextBox.TextLength;
+
+                                    FontStyle style = FontStyle.Regular;
+                                    float font_size = 12;
+                                    System.Drawing.Color color = Area_RichTextBox.SelectionColor;
+
+                                    Area_RichTextBox.Select(start, end - start);
+
+                                    if (run.RunProperties != null)
+                                    {
+                                        if (run.RunProperties.Bold != null && run.RunProperties.Bold.Val != OnOffValue.FromBoolean(true))
+                                        {
+                                            style |= FontStyle.Bold;
+                                        }
+
+                                        if (run.RunProperties.Italic != null && run.RunProperties.Italic.Val != OnOffValue.FromBoolean(true))
+                                        {
+                                            style |= FontStyle.Italic;
+                                        }
+
+                                        if (run.RunProperties.Underline != null && run.RunProperties.Underline.Val != OnOffValue.FromBoolean(true))
+                                        {
+                                            style |= FontStyle.Underline;
+                                        }
+
+                                        if (run.RunProperties.Color != null)
+                                        {
+                                            color = ColorTranslator.FromHtml("#" + run.RunProperties.Color.Val);
+                                        }
+
+                                        if (run.RunProperties.FontSize != null)
+                                        {
+                                            font_size = Convert.ToInt32(run.RunProperties.FontSize.Val) / 2f;
+                                        }
+                                    }
+
+                                    Area_RichTextBox.SelectionFont = new System.Drawing.Font(Area_RichTextBox.SelectionFont.FontFamily, font_size, style);
+                                    Area_RichTextBox.SelectionColor = color;
+
+                                    Area_RichTextBox.AppendText(text.Text);
+
+                                    Area_RichTextBox.Select(end, 0);
+                                }
+                            }
+
+                            Area_RichTextBox.AppendText(Environment.NewLine);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
